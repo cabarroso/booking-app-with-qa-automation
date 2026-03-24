a7b30e13a16772671cc0b6be1f44146288a0bc50
Christian Barroso <barrosochristian3@gmail.com>
2026-03-24 08:31:20 -0700

    parametrized missing field tests

--- tests/api/test_create_booking.py
+++ tests/api/test_create_booking.py
@@ -1,6 +1,8 @@
 from framework.services.booking_service import BookingService
 from framework.utils.data_generator import generate_booking_data
 
+import pytest
+
 def test_create_booking():
     test_booking_data = generate_booking_data()
     response = BookingService().create_booking(test_booking_data)
@@ -22,11 +24,12 @@ def test_create_booking():
     assert response_data["check_out"] == test_booking_data["check_out"]
     assert response_data["additional_needs"] == test_booking_data["additional_needs"]
 
-def test_create_booking_invalid_data():
-    invalid_data = {
-        "first_name": ""
-    }
+@pytest.mark.parametrize("field", 
+                         ["first_name", "total_price", "check_in"])
+def test_missing_field(field):
+    booking_data = generate_booking_data()
+    del booking_data[field]
 
-    response = BookingService().create_booking(invalid_data)
+    response = BookingService().create_booking(booking_data)
 
     assert response.status_code == 422

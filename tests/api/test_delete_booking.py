0ef0e750400448d9df87cf0ec9bbcc78c8001838
Christian Barroso <barrosochristian3@gmail.com>
2026-04-19 10:40:51 -0700

    api delete smoke test made deterministic and self contained

--- tests/api/test_delete_booking.py
+++ tests/api/test_delete_booking.py
@@ -1,9 +1,21 @@
 import pytest
 
+from framework.utils.helpers import assert_booking_data_matches
+
 @pytest.mark.smoke
-@pytest.mark.delete_booking
-def test_delete_booking_status(booking_service, last_booking_id):
-    response = booking_service.delete_booking(last_booking_id)
+@pytest.mark.delete
+@pytest.mark.api
+def test_delete_booking_status(booking_service, created_booking, validate_booking):
+    # SETUP - create a booking to delete
+    new_booking = created_booking["response_data"]
+
+    validate_booking(new_booking)
+
+    assert_booking_data_matches(created_booking["request_data"], new_booking)
+
+    # MAIN TESTING - delete the booking and verify status code
+
+    response = booking_service.delete_booking(new_booking["id"])
 
     assert response.status_code == 204
 

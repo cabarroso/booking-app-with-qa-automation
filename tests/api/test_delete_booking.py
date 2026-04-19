a12ac3f11dc1ae83adc7833a8b3c423e632960e5
Christian Barroso <barrosochristian3@gmail.com>
2026-04-19 11:16:14 -0700

    invalid id test more robust

--- tests/api/test_delete_booking.py
+++ tests/api/test_delete_booking.py
@@ -4,7 +4,7 @@ from framework.utils.helpers import assert_booking_data_matches
 
 @pytest.mark.delete
 @pytest.mark.api
-def test_delete_booking(booking_service, created_booking, validate_booking):
+def test_delete_booking_removes_booking(booking_service, created_booking, validate_booking):
     # SETUP - create a booking to delete
     new_booking = created_booking["response_data"]
 
@@ -16,16 +16,23 @@ def test_delete_booking(booking_service, created_booking, validate_booking):
 
     new_booking_id = new_booking["id"]
 
+    get_response = booking_service.get_booking(new_booking_id)
+    assert get_response.status_code == 200
+
     delete_response = booking_service.delete_booking(new_booking_id)
     assert delete_response.status_code == 204
 
-    response = booking_service.get_booking(new_booking_id)
-    assert response.status_code == 404
+    get_response = booking_service.get_booking(new_booking_id)
+    assert get_response.status_code == 404
 
 
 @pytest.mark.delete
 @pytest.mark.api
-def test_delete_booking_id_doesnt_exist(booking_service):
-    response = booking_service.delete_booking(0)
+def test_delete_booking_invalid_id_returns_404(booking_service):
+    get_response = booking_service.get_booking(0)
+    assert get_response.status_code == 404
 
-    assert response.status_code == 404
+    delete_response = booking_service.delete_booking(0)
+    assert delete_response.status_code == 404
+
+    assert delete_response.json() == {"detail": "Booking not found"}

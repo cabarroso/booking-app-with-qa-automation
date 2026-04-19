3e3a9d0d43d0af860a3975d10d5e7b02f809622d
Christian Barroso <barrosochristian3@gmail.com>
2026-04-19 12:24:25 -0700

    gitignore

--- framework/utils/logger.py
+++ framework/utils/logger.py
@@ -5,9 +5,13 @@ logger = logging.getLogger(__name__)
 
 def setup_logger():
     worker_id = os.environ.get("PYTEST_XDIST_WORKER")
+
     filename = "logs/test.log"
     if worker_id is not None:
         filename = f"logs/test_{worker_id}.log"
+    
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
     logging.basicConfig(filename=filename, 
                         level=logging.INFO, 
                         format="%(asctime)s | %(name)s | %(levelname)s | %(message)s")

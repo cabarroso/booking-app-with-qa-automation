6472031c4838f4a71189390b8e498efcc516b9cb
Christian Barroso <barrosochristian3@gmail.com>
2026-04-21 08:17:09 -0700

    delete redundant tests

--- tests/api/test_get_bookings.py
+++ tests/api/test_get_bookings.py
@@ -6,7 +6,7 @@ from framework.utils.helpers import assert_booking_data_matches
 @pytest.mark.bookings
 @pytest.mark.api
 @pytest.mark.smoke
-def test_get_bookings_not_empty(created_booking, booking_service, validate_booking):
+def test_get_bookings_while_not_empty(created_booking, booking_service, validate_booking):
     new_booking = created_booking["response_data"]
 
     validate_booking(new_booking)
@@ -28,25 +28,13 @@ def test_get_bookings_not_empty(created_booking, booking_service, validate_booki
 
     assert_booking_data_matches(get_booking_data, new_booking)
 
-# make sure dicts in list have relevant fields
-@pytest.mark.get_bookings
-def test_get_bookings_structure(booking_service, bookings_response):
-    # response = booking_service.get_bookings()
-    bookings = bookings_response.json()
-    
-    assert "id" in bookings[0]
-    assert "first_name" in bookings[0]
-    assert "check_in" in bookings[0]
+# @pytest.mark.get
+# @pytest.mark.bookings
+# @pytest.mark.api
+# @pytest.mark.smoke
+# def test_get_bookings_while_empty(created_booking, booking_service, validate_booking):
+#     # MUST IMPLEMENT LAST
+#     pass 
 
-# validate route contains newly created bookings
-@pytest.mark.get_bookings
-def test_get_new_booking_in_bookings(booking_service, booking_data):
-    new_booking_response = booking_service.create_booking(booking_data)
-    bookings_response = booking_service.get_bookings()
-
-    new_booking = new_booking_response.json()
-    bookings = bookings_response.json()
-
-    assert bookings[-1]["id"] == new_booking["id"]
     
     

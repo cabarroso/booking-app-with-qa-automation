f671357ead955e7ffb902b374b9f797e4591b0b4
Christian Barroso <barrosochristian3@gmail.com>
2026-04-29 16:41:12 -0700

    allure steps added to ui create booking page

--- tests/ui/test_create_booking.py
+++ tests/ui/test_create_booking.py
@@ -14,7 +14,8 @@ from pages.bookings_page import BookingsPage
 @pytest.mark.post
 @pytest.mark.booking
 def test_create_booking_successfully_loaded(create_booking_page):
-    expect(create_booking_page.heading_locator).to_be_visible()
+    with allure.step("Expect heading to be visible on page"):
+        expect(create_booking_page.heading_locator).to_be_visible()
 
 
 @allure.title("UI Create Booking Page - Successfully Create Booking")
@@ -25,42 +26,47 @@ def test_create_booking_successfully_loaded(create_booking_page):
 @pytest.mark.smoke
 @pytest.mark.booking
 def test_create_booking(create_booking_page, booking_service, booking_data, validate_booking):
-    # Catch post response info from POST
-    with create_booking_page.page.expect_response("**/bookings") as response_info:
-        create_booking_page.create_booking(booking_data)
+    with allure.step("Create a new booking via the form on the page"):
+        # Catch post response info from POST
+        with create_booking_page.page.expect_response("**/bookings") as response_info:
+            create_booking_page.create_booking(booking_data)
 
     response = response_info.value
-    assert response.status == 201
+
+    with allure.step("Verify success with status of 201"):
+        assert response.status == 201
 
     new_booking = response.json()
 
     print(f"[SETUP] Created booking ID={new_booking['id']}")
 
-    validate_booking(new_booking)
+    with allure.step("Validate new booking against booking schema"):
+        validate_booking(new_booking)
 
-    assert_booking_data_matches(new_booking, booking_data)
+    with allure.step("Verify new booking matches test data"):
+        assert_booking_data_matches(new_booking, booking_data)
 
     # wait for /bookings redirect
     create_booking_page.page.wait_for_load_state("networkidle")
 
-    # confirm redirected away from create booking page
-    expect(create_booking_page.heading_locator).not_to_be_visible()
-
-    # verify new booking is present and visible in UI with correct data
-    bookings_page = BookingsPage(create_booking_page.page)
-    
-    # confirm redirect to /bookings
-    expect(bookings_page.heading_locator).to_be_visible()
+    with allure.step("Expected to be redirected to bookings page"):
+        # confirm redirected away from create booking page
+        expect(create_booking_page.heading_locator).not_to_be_visible()
+        bookings_page = BookingsPage(create_booking_page.page)
+        # confirm redirect to /bookings
+        expect(bookings_page.heading_locator).to_be_visible()
 
     new_booking_id = new_booking["id"]
 
-    # verify new booking is present and visible in UI...
-    assert bookings_page.booking_row_is_present(new_booking_id)
-    expect(bookings_page.get_booking_row_by_id(new_booking_id)).to_be_visible()
+    with allure.step("Verify new booking row is present and visible in UI"):
+        # verify new booking is present and visible in UI...
+        assert bookings_page.booking_row_is_present(new_booking_id)
+        expect(bookings_page.get_booking_row_by_id(new_booking_id)).to_be_visible()
 
-    # ...with correct data
-    new_booking_ui_data = bookings_page.get_booking_data_by_id(new_booking_id)
-    assert_booking_data_matches(new_booking_ui_data, new_booking)
+    with allure.step("Verify new booking row has correct data"):
+        # ...with correct data
+        new_booking_ui_data = bookings_page.get_booking_data_by_id(new_booking_id)
+        assert_booking_data_matches(new_booking_ui_data, new_booking)
 
     # CLEANUP - delete the created booking
 

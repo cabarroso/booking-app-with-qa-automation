78788ba8928e14337c59c1d1ebab536397e0e35d
Christian Barroso <barrosochristian3@gmail.com>
2026-05-01 08:03:13 -0700

    allure steps added to ui login page

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -10,7 +10,8 @@ from playwright.sync_api import expect
 @pytest.mark.login
 @pytest.mark.smoke
 def test_login_page_loaded(login_page):
-    expect(login_page.heading_locator).to_be_visible()
+    with allure.step("Expect login page heading to be visible"):
+        expect(login_page.heading_locator).to_be_visible()
 
 @allure.title("UI Login Page - Successful Login")
 @allure.description("User is able to successfully login with valid credentials.")
@@ -18,13 +19,18 @@ def test_login_page_loaded(login_page):
 @pytest.mark.ui
 @pytest.mark.login
 def test_login(login_page):
-    login_page.login("admin", "password123")
-    expect(login_page.heading_locator).not_to_be_visible()
-    assert not login_page.at_login_page
-    expect(login_page.error_message_locator).not_to_be_visible()
+    with allure.step("Login via UI with valid credentials"):
+        login_page.login("admin", "password123")
+    
+    with allure.step("Verify page redirects away from login page"):
+        expect(login_page.heading_locator).not_to_be_visible()
+        assert not login_page.at_login_page
 
-@allure.title("UI Login Page - Failed Login with invalid Credentials")
-@allure.description("User fails to login with invalid credentials.")
+    with allure.step("Expect error message not to be visible"):
+        expect(login_page.error_message_locator).not_to_be_visible()
+
+@allure.title("UI Login Page - Failed Login with Empty Input Value")
+@allure.description("User fails to login with empty username or password values")
 @allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.ui
 @pytest.mark.login
@@ -32,13 +38,17 @@ def test_login(login_page):
                          [
                              ("", "password123"),
                              ("admin", ""),
-                             ("", "")
                          ],
-                         ids=["no username", "no password", "no username or password"]
+                         ids=["username", "password"]
                         )
-def test_login_invalid_credentials(login_page, username, password):
-    login_page.login(username, password)
-    expect(login_page.heading_locator).to_be_visible()
-    assert login_page.at_login_page
-    expect(login_page.error_message_locator).to_be_visible()
+def test_login_empty_input(login_page, username, password):
+    with allure.step("Attempt login with an empty field value"):
+        login_page.login(username, password)
+
+    with allure.step("Verify that we are still at the login page"):
+        expect(login_page.heading_locator).to_be_visible()
+        assert login_page.at_login_page
+
+    with allure.step("Expect error message to be visible"):
+        expect(login_page.error_message_locator).to_be_visible()
 
